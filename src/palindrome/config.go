216f35f2a01636dc5032a0c932d26049e5d5e815
Mavrick Laakso <laakso.mavrick@gmail.com>
2019-09-20 14:18:36 -0400

    chore/scaffold-project

---
+++ src/palindrome/config.go
@@ -0,0 +1,21 @@
+package palindrome
+
+import "os"
+
+type Config struct {
+	// TODO: reconcile public/private
+	goEnv string
+	port string
+	PostgresUser string
+	PostgresHost string
+}
+
+func NewConfig() *Config {
+	// TODO: log error if one of these values is null
+	return &Config{
+		goEnv: os.Getenv("GO_ENV"),
+		port: os.Getenv("PORT"),
+		PostgresUser: os.Getenv("POSTGRES_USER"),
+		PostgresHost: os.Getenv("POSTGRES_HOST"),
+	}
+}
